908fde0469e0ac0225dd388ac4a79caa84cec1e3
Andarna <kimondo2005@gmail.com>
2025-09-14 22:43:38 +0300

    Add basic_operations.py with arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+# Predefined numbers
+number1=10
+numbe2=5
+
+# Perform and print operations
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
+
